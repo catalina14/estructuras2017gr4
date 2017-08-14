3a753f462a47d59e50b7d76a39f8b331d24c8de6
iycastellanosm <iycastellanosm@unal.edu.co>
2017-08-13 23:12:41 -0500

    listas

---
+++ LinearList.java
@@ -0,0 +1,10 @@
+
+public interface LinearList <T>{
+    boolean isEmpty();
+    int size();
+    T get(int i);
+    int indexOf(T x);
+    T remove(int i);
+    void add(int i, T x);
+    String toString();
+}
